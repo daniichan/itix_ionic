1a8c9fc2468edbe1df2b8c8df15df15e9c3b0cec
Daniele Leite <danii.exe@gmail.com>
2020-05-14 02:50:18 -0300

    Developed aniversarios page.

---
+++ src/app/pages/aniversarios/aniversarios.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aniversarios',
+  templateUrl: './aniversarios.page.html',
+  styleUrls: ['./aniversarios.page.scss'],
+})
+export class AniversariosPage implements OnInit {
+
+  aniversariantes: string[] = [
+    'José da Silva Teixeira',
+    'Luiz Carlos Garcia',
+    'Maria Aparecida Vieira Couto',
+    'Roberto Almeida Spoletti',
+    'Ana Maria Cerqueira',
+    'Pedro Arthur Nogueira Rispinni',
+    'Francine Fonseca Pedrini'
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+
+}
